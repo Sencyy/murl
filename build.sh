88493eead36d05a4679a24afcddbba07ce49b4fb
Sencyy <sencyy@protonmail.com>
2026-03-12 14:36:28 -0300

    update build script

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-#! /usr/bin/env bash
+#! /bin/sh
 
 mkdir -pv build/data
+mkdir -v build/bin
 
 cd src/backend
-cc -o murl main.c
+cc -o murl main.c -DWE_ARE_ON_NIX
 cd ../..
 
 cp -rv contrib build
 cp -rv src/frontend build
-mv -v src/backend/murl build
+mv -v src/backend/murl build/bin
